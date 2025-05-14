79071c6e13121bf5f1f506619a32ec4a19f91a5d
CojoCosta <dcosta1882000@gmail.com>
2025-05-14 13:58:26 +0200

    changes

--- 3ª EV/boletin_junit_diagramacion/src/test/java/ejercicios/Ej2.java
+++ 3ª EV/boletin_junit_diagramacion/src/test/java/ejercicios/Ej2.java
@@ -8,8 +8,14 @@ public class Ej2 {
     @Test
     public void comprobarSubCadena(){
         assertEquals("hola",Ejercicio2.subCadena("hola", 0, 4) );
-        Ejercicio2.subCadena("hola", 0, 5);
+        assertEquals("hola",Ejercicio2.subCadena("hola", 0, 5));
         assertEquals("a",Ejercicio2.subCadena("hola", 3, 1));
-        Ejercicio2.subCadena("hola", 4, 1);
+        assertEquals("",Ejercicio2.subCadena("hola", 4, 1));
+        assertEquals("la",Ejercicio2.subCadena("hola", 2, 2));
+        assertEquals("ol",Ejercicio2.subCadena("hola", -1, 2));
+        assertEquals("ol",Ejercicio2.subCadena("hola", 2, -1));
     }
+
+    
 }
+
