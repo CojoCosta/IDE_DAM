cebc0f3e742a8d45ba9f6cf31aa25b7772d88bf7
CojoCosta <dcosta1882000@gmail.com>
2025-05-16 12:15:22 +0200

    changes

--- 3ª EV/boletin_junit_diagramacion/src/test/java/ejercicios/Ej3.java
+++ 3ª EV/boletin_junit_diagramacion/src/test/java/ejercicios/Ej3.java
@@ -1,16 +1,47 @@
 package ejercicios;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class Ej3 {
+    int[] numeros;
+
+    @BeforeEach
+    void AntesDeCadaUno() {
+        numeros = new int[] {1, 2, 3, 4, 0};
+    }
     @Test
-    public void testNumeroMayor(){
-        int [] numeros = {5, 6, 15, 95, 25 , 99, 54, 44, 36, 72};
-        for (int i = 0; i < numeros.length; i++) {
-            assert
-        }
+    public void testNumeroMayor() {
+        assertEquals(4, Ejercicio3.numeroMayor(numeros));
+    }
+    
+    @Test
+    public void testNumeroMenor() {
+        assertEquals(0, Ejercicio3.numeroMenor(numeros));
+    }
+
+    @Test
+    public void textIntercambioPosiciones() {
+        assertTrue(Ejercicio3.intercambioPosiciones(numeros, 2, 4));
+    }
+    
+    //#region MOCKS
+    public int[] ordenaVector(int[] v){
+            return v;
+    }
+    
+    public int sumaRango(int[] v, int inicio, int fin){
+        return 0;
+    }
+    //#endregion
+    
+    @Test
+    public void testOrdenaVector(){
+        assertArrayEquals(new int[] {0, 1, 2, 3, 4}, ordenaVector(numeros), "Se ordena");
     }
 }
 
