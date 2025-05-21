14b755f9c8a673507637cdc6acbe9e61a6c3b89f
CojoCosta <dcosta1882000@gmail.com>
2025-05-21 13:06:33 +0200

    changes

--- 3ª EV/boletin_junit_diagramacion/src/test/java/ejercicios/Ej3.java
+++ 3ª EV/boletin_junit_diagramacion/src/test/java/ejercicios/Ej3.java
@@ -9,23 +9,52 @@ import org.junit.jupiter.api.Test;
 
 public class Ej3 {
     int[] numeros;
+    int[] numeros1;
+    int[] numeros2;
 
     @BeforeEach
     void AntesDeCadaUno() {
         numeros = new int[] {1, 2, 3, 4, 0};
+        numeros1 = new int[] {4, 4, 4, 4, 4};
+        numeros2 = new int[] {-1, -2, -3, -4, -5};
     }
     @Test
     public void testNumeroMayor() {
         assertEquals(4, Ejercicio3.numeroMayor(numeros));
+        assertEquals(4, Ejercicio3.numeroMayor(numeros1));
+        assertEquals(-1, Ejercicio3.numeroMayor(numeros2));
     }
     
     @Test
     public void testNumeroMenor() {
         assertEquals(0, Ejercicio3.numeroMenor(numeros));
+        assertEquals(4, Ejercicio3.numeroMenor(numeros1));
+        assertEquals(-5, Ejercicio3.numeroMenor(numeros2));
+    }
+    @Test
+    public void testNumeroMenorFails() {
+        try {
+            assertEquals(3, Ejercicio3.numeroMenor(numeros));
+        } catch (IllegalArgumentException e) {
+            System.out.println("Expected equivocado");
+        }
+        try {
+            assertEquals(3, Ejercicio3.numeroMenor(numeros1));
+        } catch (IllegalArgumentException e) {
+            System.out.println("Expected equivocado");
+        }
+        try {
+            assertEquals(-1, Ejercicio3.numeroMenor(numeros2));
+        } catch (IllegalArgumentException e) {
+            System.out.println("Expected equivocado");
+        }
     }
 
     @Test
     public void textIntercambioPosiciones() {
+        assertTrue(Ejercicio3.intercambioPosiciones(numeros, 2, 4));
+        assertTrue(Ejercicio3.intercambioPosiciones(numeros, 0, 4));
+        assertTrue(Ejercicio3.intercambioPosiciones(numeros, 4, 0));
         assertTrue(Ejercicio3.intercambioPosiciones(numeros, 2, 4));
     }
     
